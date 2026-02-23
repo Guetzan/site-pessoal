86c34981942ebe20ea2f681c0589b6f85eedc1bd
Clovis <orcsdzn@gmail.com>
2026-02-23 00:38:41 -0300

    adiciona versao utilizavel do site

--- main.js
+++ main.js
@@ -1,23 +1,46 @@
-let currentActiveItem = document.querySelector('li.selected');
-const navItems = document.querySelectorAll('.nav-item > a');
-
 window.onload = () => {
-    currentActiveItem.classList.remove('selected')
-    currentActiveItem.classList.add('selected')
-};
+    switchDisplayContent(window.location.hash);
+}
 
-navItems.forEach(navItem => {
-    navItem.addEventListener('click', () => {
-        if(navItem === currentActiveItem)
-            return;
-        
-        if(navItem === navItems.item(0))
-            return;
+window.onhashchange = () => {
+    switchDisplayContent(window.location.hash);
+}
 
-        currentActiveItem.classList.remove('selected');
-        navItem.parentElement.classList.add('selected');
-        
-        currentActiveItem = navItem.parentElement;
-    });
-});
+let contentOnDisplay = document.querySelector('.content.showing');
 
+const secondaryNav = document.querySelector('nav.secondary');
+const display = document.getElementById('content-display');
+const teste = document.getElementById("teste");
+
+function switchDisplayContent(hash) {
+    const content = document.querySelector(hash);
+
+    if(content === contentOnDisplay) 
+        return;
+
+    if(content === document.querySelector('#inicio'))
+        secondaryNav.style.opacity = 0;
+    
+    if(contentOnDisplay === document.querySelector('#inicio'))
+        secondaryNav.style.opacity = 1;
+
+
+    contentOnDisplay.classList.remove('showing');
+    content.classList.add('showing');
+    contentOnDisplay = content;
+
+    updateSecondaryNav();
+}
+
+let selectedNavItem;
+
+function updateSecondaryNav() {
+    if(selectedNavItem) {
+        selectedNavItem.classList.remove('selected');
+    }
+
+    selectedNavItem = secondaryNav.querySelector(`a[href="${window.location.hash}"]`).parentElement;
+
+
+    selectedNavItem.classList.add('selected');
+}
